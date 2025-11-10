a978707a12c4b426e6a0407bc1fab0c6fa7d647c
JISU <136044106+qpry42@users.noreply.github.com>
2025-11-10 17:44:32 +0900

    Title: 삼항연산자 2, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 251110/삼항연산자 2/ternary-operator-2.py
+++ 251110/삼항연산자 2/ternary-operator-2.py
@@ -1,6 +1,9 @@
 a = int(input())
 
-if a == 1:
-    print("t")
-else:
-    print("f")
+#if a == 1:
+#    print("t")
+#else:
+#    print("f")
+
+#삼항연산자를 이용한 코드
+print('t') if int(input()) == 1 else print('f')
