aaccfaa0ca8cab4b51e69a50d11f8e8592ddc525
JISU <136044106+qpry42@users.noreply.github.com>
2025-11-15 23:39:56 +0900

    Title: 알파벳에 따른 평가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251115/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+Eng = input()
+
+if Eng == "S":
+    print("Superior")
+elif Eng == "A":
+    print("Excellent")
+elif Eng == "B":
+    print("Good")
+elif Eng == "C":
+    print("Usually")
+elif Eng == "D":
+    print("Effort")
+else:
+    print("Failure")
