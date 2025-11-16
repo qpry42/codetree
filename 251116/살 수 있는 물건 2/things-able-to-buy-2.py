1f922138ad4a497bbf7300c1d7c170b55d863feb
JISU <136044106+qpry42@users.noreply.github.com>
2025-11-16 10:25:37 +0900

    Title: 살 수 있는 물건 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251116/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -0,0 +1,14 @@
+book = 3000
+mask = 1000
+pen = 500
+
+N = int(input())
+
+if N >= 3000:
+    print("book")
+elif N >= 1000:
+    print("mask")
+elif N >= 500:
+    print("pen")
+else:
+    print("no")
