86496b93121ac79c148a95ce589f8cc4900b2b56
JISU <136044106+qpry42@users.noreply.github.com>
2025-12-02 20:53:39 +0900

    Title: 특정 조건 두 정수 비교, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251202/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 251202/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -11,3 +11,19 @@ else:
     S = 0
 
 print(N, S)
+
+# 변수 선언, 입력
+#inp = input()
+#arr = inp.split()
+#a = int(arr[0])
+#b = int(arr[1])
+# 출력
+#if a < b:
+#	print("1", end=" ")
+#else:
+#	print("0", end=" ")
+#if a == b:
+#	print("1")
+#else:
+#	print("0")
+
