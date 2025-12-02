ee2673227d9e94d74617d385746af7cc8719f63d
JISU <136044106+qpry42@users.noreply.github.com>
2025-12-02 20:58:03 +0900

    Title: 특정 조건 두 정수 비교, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251202/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 251202/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -12,18 +12,23 @@ else:
 
 print(N, S)
 
-# 변수 선언, 입력
-#inp = input()
-#arr = inp.split()
-#a = int(arr[0])
-#b = int(arr[1])
-# 출력
-#if a < b:
-#	print("1", end=" ")
-#else:
-#	print("0", end=" ")
-#if a == b:
-#	print("1")
-#else:
-#	print("0")
+''' 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
 
+# 출력
+if a < b:
+	print("1", end=" ")
+else:
+	print("0", end=" ")
+if a == b:
+	print("1")
+else:
+	print("0")
+
+✨ end=" "의 역할
+
+print("1", end=" ")는
+출력 결과를 1 뒤에 공백을 붙이고 줄바꿈을 하지 않는다는 뜻이야.'''
