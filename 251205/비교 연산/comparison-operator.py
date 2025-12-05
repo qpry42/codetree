836ffbe3b163e44b7b27a7ed3b87e526fa211aa2
JISU <136044106+qpry42@users.noreply.github.com>
2025-12-05 23:47:44 +0900

    Title: 비교 연산, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251205/비교 연산/comparison-operator.py
+++ 251205/비교 연산/comparison-operator.py
@@ -1,12 +1,9 @@
 A, B = map(int, input().split())
 
-if False:
-    print(0)
-else:
-    print(1)
-print(A >= B)
-print(A > B)
-print(B >= A)
-print(B > A)
-print(A == B)
-print(A != B)
+
+print(int(A >= B))
+print(int(A > B))
+print(int(B >= A))
+print(int(B > A))
+print(int(A == B))
+print(int(A != B))
