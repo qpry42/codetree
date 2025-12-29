dec80451dd0b7e7296d9a6b6f7fd040d31679ccc
JISU <136044106+qpry42@users.noreply.github.com>
2025-12-29 20:27:57 +0900

    Title: 수의 계절은, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251229/수의 계절은/season-of-num.py
@@ -0,0 +1,10 @@
+M = int(input())
+
+if M >=3 and M <= 5:
+    print("Spring")
+if M >=6 and M <= 8:
+    print("Summer")
+if M >=9 and M <= 11:
+    print("Fall")
+if M >=12 or 1 <= M <= 2:
+    print("Winter")
