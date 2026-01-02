ef514934ec81e186710d0d8fec0980587b647213
JISU <136044106+qpry42@users.noreply.github.com>
2026-01-02 23:44:49 +0900

    Title: 좀 더 어려운 수학 점수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260102/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 260102/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -10,3 +10,17 @@ if A_Math == B_Math and A_Eng > B_Eng:
     print("A")
 elif A_Math == B_Math and A_Eng < B_Eng:
     print("B")
+
+
+"""A_math, A_eng = map(int, input().split())
+B_math, B_eng = map(int, input().split())
+
+if A_math > B_math :
+    print("A")
+elif B_math > A_math :
+    print("B")
+else :
+    if A_eng > B_eng :
+        print("A")
+    else :
+        print("B")"""
