8de9ff4aa217de6a3e782b1c14f39303c4eebd65
JISU <136044106+qpry42@users.noreply.github.com>
2026-01-04 23:01:15 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260104/윤년인가/is-leap-year.py
+++ 260104/윤년인가/is-leap-year.py
@@ -1,8 +1,18 @@
-Y = int(input())
+# 변수 선언, 입력
+y = int(input())
 
-if Y % 4 != 0:
-    print("false")
-elif Y % 100 == 0 and Y % 400:
-    print("false")
+# 출력
+if y % 4 == 0:
+	if y % 100 == 0:
+		if y % 400 == 0:
+			print("true")  #4, 100, 400 나머지가 0일 때 윤년
+		else:
+			print("false") #400은 안될 때 평년
+	else:
+		print("true") #4로만 나뉠 때 윤년
 else:
-    print("true")
+	print("false") #4도 안될때
+
+
+
+
