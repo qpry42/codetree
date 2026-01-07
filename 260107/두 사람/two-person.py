6e2baed280e0cd807f14747d43a0ceeb3786f7a2
JISU <136044106+qpry42@users.noreply.github.com>
2026-01-07 21:19:16 +0900

    Title: 두 사람, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260107/두 사람/two-person.py
@@ -0,0 +1,14 @@
+A_age, A_sex = input().split()
+B_age, B_sex = input().split()
+
+A_age = int(A_age)
+B_age = int(B_age)
+A_sex = str(A_sex)
+B_sex = str(B_sex)
+
+if A_age >= 19 or B_age >= 19:
+    if A_sex == "M" or B_sex == "M":
+        print(1)
+    else:
+        print(0)
+        
