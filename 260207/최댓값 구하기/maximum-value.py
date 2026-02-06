582dd38f872e60c318f24a42cbff5a4a495fabfd
Jisu Han <136044106+expJisuHan@users.noreply.github.com>
2026-02-07 01:27:22 +0900

    Title: 최댓값 구하기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260207/최댓값 구하기/maximum-value.py
+++ 260207/최댓값 구하기/maximum-value.py
@@ -2,7 +2,7 @@ a, b, c = map(int, input().split())
 
 if a >= b and b >= c:
     print(a)
-elif a == b and b >= c:
-    print(b)
-elif a == b == c:
+elif a == b and b >= c or a <= b and b >= c:
+    print(b) 
+else:
     print(c)
